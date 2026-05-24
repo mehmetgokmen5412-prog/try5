2ba697414b9e42da07da05300e7829132652888c
mehmetgokmen5412-prog <mehmetgokmen5412@gmail.com>
2026-05-25 01:23:47 +0300

    a
    a

--- Assets/SwordCombat.cs
+++ Assets/SwordCombat.cs
@@ -1,40 +1,56 @@
 using UnityEngine;
+using System.Collections;
 
 public class SwordCombat : MonoBehaviour
 {
-    public Transform attackPoint; // Kılıcın sallandığı nokta (boş bir GameObject oluşturup karakterin eline koy)
-    public float attackRange = 0.5f; // Kılıcın erişim mesafesi
-    public LayerMask enemyLayers; // Düşmanların olduğu Layer (Inspector'dan "Enemy" katmanını seç)
+    [Header("Saldırı Ayarları")]
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public float attackCooldown = 1.5f;
+
+    [Header("Animasyon Ayarları")]
+    public Animator anim;
+
+    private bool isAttacking = false;
 
     void Update()
     {
-        // F tuşuna basınca saldır
-        if (Input.GetKeyDown(KeyCode.F))
+        // Karakterin yönünü al (Scale X değeri 1 ise sağ, -1 ise sol)
+        float horizontal = transform.localScale.x;
+
+        // AttackPoint'in Y ve Z'sini sabit tut, X'i yönle çarp
+        // 0.5f değerini kılıcın uzaklığına göre 0.7f veya 0.8f yapabilirsin
+        attackPoint.localPosition = new Vector3(0.5f * (horizontal > 0 ? 1 : -1), 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.F) && !isAttacking)
         {
-            Attack();
+            StartCoroutine(AttackRoutine());
         }
     }
 
-    void Attack()
+    IEnumerator AttackRoutine()
     {
-        // 1. Animasyonu oynat (Animator kullanıyorsan)
-        // GetComponent<Animator>().SetTrigger("Attack");
+        isAttacking = true;
+        if (anim != null) anim.SetTrigger("Attack");
 
-        // 2. Saldırı noktasında çember oluştur ve içine giren düşmanları bul
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
 
-        // 3. Yakalanan düşmanlara hasar ver
         foreach (Collider2D enemy in hitEnemies)
         {
-            Debug.Log(enemy.name + " hasar aldı!");
-            // enemy.GetComponent<EnemyHealth>().TakeDamage(10); // Düşman scriptindeki hasar alma fonksiyonunu çağır
+            Debug.Log("Vurulan: " + enemy.name);
         }
+
+        yield return new WaitForSeconds(attackCooldown);
+        isAttacking = false;
     }
 
-    // Sahne görünümünde saldırı alanını görmek için
     void OnDrawGizmosSelected()
     {
-        if (attackPoint == null) return;
-        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        if (attackPoint != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        }
     }
 }
